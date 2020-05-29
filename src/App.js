f881a64edab26d24b5c34bff352ba68e1c9f90ca
Ayden Cauchi <ayden@modimages.com>
2020-05-29 17:13:11 -0400

    cards complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 import {Cards, Chart, CountryPicker} from './components'
+import {fetchData} from './api/index';
+import styles from './App.module.css';
 
 class App extends React.Component {
+    state = {
+        data: {},
+    };
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    };
+
     render(){
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards data={this.state.data}/>
+                <CountryPicker />
+                <Chart />
             </div>
         );
-    }
-}
+    };
+};
 export default App;
