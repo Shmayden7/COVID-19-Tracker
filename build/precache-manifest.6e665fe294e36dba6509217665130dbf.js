8960d2507fe824539336ad8c0c540666fbb2c073
Ayden Cauchi <ayden@modimages.com>
2020-05-29 20:57:06 -0400

    build included

---
+++ build/precache-manifest.6e665fe294e36dba6509217665130dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0c7e6418a18d7b1d9e8f8600ac9c96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1593c22f76004ce89a47",
+    "url": "/static/css/main.d6ca8012.chunk.css"
+  },
+  {
+    "revision": "11c5cc6adca7af64c02b",
+    "url": "/static/js/2.2322da0f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.2322da0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1593c22f76004ce89a47",
+    "url": "/static/js/main.608c67e4.chunk.js"
+  },
+  {
+    "revision": "84599ef62b7876584165",
+    "url": "/static/js/runtime-main.5948f688.js"
+  },
+  {
+    "revision": "a4f88d05f77d487eb552530c4db1ccd4",
+    "url": "/static/media/image.a4f88d05.png"
+  }
+]);
